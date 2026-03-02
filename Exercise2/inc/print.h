f9c05f9e1b59e61de613ed474eb85e7676038d30
Simon Westlin Green <simonwestlingreen@proton.me>
2026-03-02 12:53:04 +0100

    cleanup: remove exercise

--- Exercise2/inc/print.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PRINT_H
-#define _PRINT_H
-
-void print(char *format, ...);
-
-#endif
--- Exercise2/src/objects.c
+++
@@ -1,5 +0,0 @@
-#include "App.h"
-#include "sciTinyTimber.h"
-
-App app = initApp();
-Serial sci0 = initSerial(SCI_PORT0, &app, reader);
